33de7df8027ef6d5ed43f9342a19b841202d4947
unknown <shrutikannadate03@gmail.com>
2021-03-18 10:10:37 +0530

    filters

--- filter.js
+++ filter.js
@@ -1,13 +1,14 @@
 /* Set the width of the side navigation to 260px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
     document.getElementById("mySidenav").style.width = "260px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor="grey";
+    document.getElementById("mySidenav").style.backgroundColor="white";
+    document.getElementById("filterbutton").style.backgroundColor="white";
   }
   
   /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
     document.body.style.backgroundColor = "white";
   }
   function myFunction() {
