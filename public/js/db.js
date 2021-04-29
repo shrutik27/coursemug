862203b57c8e19f769f71bed6aedcfa02b5c865d
unknown <shrutikannadate03@gmail.com>
2021-04-29 22:05:19 +0530

    aws model

--- public/js/db.js
+++ public/js/db.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql2');
 const dbConnection = mysql.createPool({
-    host     : 'sql264.main-hosting.eu', // MYSQL HOST NAME
-    user     : 'u139817065_shareeasy', // MYSQL USERNAME
-    password : '9822174187@Ss', // MYSQL PASSWORD
-    database : 'u139817065_shareeasy' ,//MYSQL DATABASENAME
+    host     : 'course.c7oqi1rod4zk.us-west-2.rds.amazonaws.com', // MYSQL HOST NAME
+    user     : 'shrutik', // MYSQL USERNAME
+    password : '9822174187', // MYSQL PASSWORD
+    database : 'course' ,//MYSQL DATABASENAME
 }).promise();
 module.exports = dbConnection;
